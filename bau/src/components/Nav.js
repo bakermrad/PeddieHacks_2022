fc32791ff5e4c45d7e46910cffdc1d487cf42ced
bakermrad2 <bakermrad0@gmail.com>
2022-08-20 04:04:16 +0300

    basic_setup

---
+++ bau/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Nav() {
+    return (
+        <nav   >
+            {/* <img src="logo.png" alt=""> */}
+
+            <img src="./images/CetusLogoWhite.png" alt="dfgh" className="logo" />
+            <li className="nav-li"><a href="#problem">PROBLEM</a></li>
+            <li className="nav-li"><a href="#solution">SOLUTION</a></li>
+            <li className="nav-li"><a href="#project--con">PROJECT</a></li>
+
+
+        </nav>
+    )
+}
