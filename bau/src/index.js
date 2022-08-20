7793ae3f78e5ee9d39749583b9ab6d4276301d8a
bakermrad2 <bakermrad0@gmail.com>
2022-08-20 04:23:56 +0300

    0.1

--- bau/src/index.js
+++ bau/src/index.js
@@ -1,5 +1,4 @@
 import React from "react"
 import ReactDOM from 'react-dom';
 import App from "./App"
-import "./style.css"
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))   
