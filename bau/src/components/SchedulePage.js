9c316f155f23c0e578225c894cb99017d58726a4
bakermrad2 <bakermrad0@gmail.com>
2022-08-21 11:33:14 +0300

    123

--- bau/src/components/SchedulePage.js
+++ bau/src/components/SchedulePage.js
@@ -2,15 +2,102 @@ import React from 'react'
 
 const SchedulePage = () => {
     return (
-        <div>
-            <h1> 5 Day Push, Pull, Legs </h1>
+        <div className='SchedulePage'>
+            <h1 className='workout-Title'> 5 Day Push, Pull, Legs </h1>
             <div className="days">
                 <div className="dayCard">
                     <div className="day">
-                        <h3>Monday</h3>
+                        <h3 className='center'>Monday</h3>
                     </div>
                     <div className="muscle">
-                        <h3>Chest , Triceps</h3>
+                        <h3 className='center'>Chest , Triceps</h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Tuesday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Back , Biceps</h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Wednesday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Legs</h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Thursday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Chest, Shoulders</h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Saturday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Back</h3>
+                    </div>
+                </div>
+            </div>
+            <h1 className='workout-Title' > Full Body 3 Day Split </h1>
+            <div className="days">
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Monday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Full Body</h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Wednesday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Full Body</h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Saturday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Full Body</h3>
+                    </div>
+                </div>
+            </div>
+
+            <h1 className='workout-Title' > UPPER LOWER 3 DAY SPLIT </h1>
+            <div className="days">
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Monday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Upper </h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Wednesday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Lower</h3>
+                    </div>
+                </div>
+                <div className="dayCard">
+                    <div className="day">
+                        <h3 className='center'>Saturday</h3>
+                    </div>
+                    <div className="muscle">
+                        <h3 className='center'>Upper</h3>
                     </div>
                 </div>
             </div>
